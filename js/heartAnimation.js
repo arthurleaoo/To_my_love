ac4515bfcdcfc7c55b850fd8fa543e673252026c
rcesar99 <ruancesar159@gmail.com>
2024-11-21 16:58:06 -0300

    texto dinâmico [alteração JS e adição de animação no CSS]

--- js/heartAnimation.js
+++ js/heartAnimation.js
@@ -1,3 +1,30 @@
+// Função para o efeito de digitação
+export function typeText(element, text, speed, callback) {
+  let index = 0;
+  // Criação do cursor
+  const cursor = document.createElement("span");
+  cursor.textContent = "|";
+  cursor.style.cssText = `
+  display: inline-block;
+  margin-left: 5px;
+  color: inherit;
+  animation: blink 0.8s steps(1) infinite;
+`;
+  element.appendChild(cursor);
+
+  const interval = setInterval(() => {
+    if (index < text.length) {
+      element.textContent = text.slice(0, index + 1); // Atualiza o texto dinamicamente
+      element.appendChild(cursor); // Reanexa o cursor ao final
+      index++;
+    } else {
+      clearInterval(interval);
+
+      if (callback) callback(); // Chama a animação dos corações após o texto ser concluído
+    }
+  }, speed);
+}
+
 // Função para gerar um número aleatório entre dois valores
 const randomNum = (x, y) => {
   x = parseInt(x);
@@ -5,11 +32,8 @@ const randomNum = (x, y) => {
   return Math.floor(Math.random() * (y - x + 1)) + x;
 };
 
-// Função para criar a animação de coraçõeszinhos
 export const heartAnimation = () => {
   const effectText = document.querySelector(".text-effect");
-  if (!effectText) return;
-
   const heartCount = (effectText.offsetWidth / 50) * 5;
 
   for (let i = 0; i < heartCount; i++) {
@@ -18,7 +42,7 @@ export const heartAnimation = () => {
     const heart = document.createElement("span");
     heart.classList.add("tiny-heart");
 
-    // Define os estilos inline para o coração
+    // Define os estilos para o coração
     heart.style.top = `${randomNum(40, 80)}%`;
     heart.style.left = `${randomNum(0, 100)}%`;
     heart.style.width = `${heartSize}px`;
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import { SlideNav } from "./slide.js";
-import { heartAnimation } from "./heartAnimation.js";
+import { typeText, heartAnimation } from "./heartAnimation.js";
 import { dateCount } from "./countDays.js";
 
 // Slides de Imagens
@@ -7,10 +7,15 @@ const slides = new SlideNav(".slide-item", ".slides-all");
 slides.init();
 slides.addControl();
 
-// Animação dos corações no título
-document.addEventListener("DOMContentLoaded", () => {
-  heartAnimation();
-});
-
 // Contagem dinâmica de dias
 dateCount();
+
+// Animação de "typing" ao h1 principal
+document.addEventListener("DOMContentLoaded", () => {
+  const h1 = document.querySelector(".text-effect");
+  const text = "Ruan & Alyne.";
+  const typingSpeed = 200; // Velocidade de digitação (ms por letra)
+
+  // Inicia o efeito de digitação
+  typeText(h1, text, typingSpeed, heartAnimation);
+});
